e3aaac75188f0740c9aae3df741a172b96fc53d2
Mohammad Saud Khan <117519529+godspeed2134@users.noreply.github.com>
2024-04-29 20:43:58 +0530

    Add files via upload

---
+++ Day1/addition_by_input.py
@@ -0,0 +1,4 @@
+num1 = int(input("1st number = "))
+num2 = int(input("2nd number = "))
+
+print("Output : ", num1 + num2)
---
+++ Day1/brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand name generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Name of you band : ", city + pet)
